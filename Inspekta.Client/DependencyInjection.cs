34022e1012eb67d86cd86d69f9c7abace16a578e
Mikołaj Szczepek <m.szczepek01@gmail.com>
2025-11-17 22:57:53 +0100

    Create translations system

--- Inspekta.Client/DependencyInjection.cs
+++ Inspekta.Client/DependencyInjection.cs
@@ -3,6 +3,7 @@ using Blazored.Toast;
 using FluentValidation;
 using Inspekta.Client.Providers;
 using Microsoft.AspNetCore.Components.Authorization;
+using System.Globalization;
 
 namespace Inspekta.Client;
 
@@ -10,35 +11,55 @@ public static class DependencyInjection
 {
 	public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
 	{
-		string baseUrl = configuration["URL:BaseUrl"] ?? string.Empty;
+        #region Http
+        string baseUrl = configuration["URL:BaseUrl"] ?? string.Empty;
 
-		services.AddTransient<JwtAuthorizationMessageHandler>();
+        services.AddTransient<JwtAuthorizationMessageHandler>();
 
-		services.AddHttpClient("Inspekta", client =>
-		{
-			client.BaseAddress = new Uri(baseUrl);
-		})
-		.AddHttpMessageHandler<JwtAuthorizationMessageHandler>();
 
-		services.AddScoped(sp =>
-			sp.GetRequiredService<IHttpClientFactory>().CreateClient("Inspekta")
-		);
+        services.AddHttpClient("Inspekta", client =>
+        {
+            client.BaseAddress = new Uri(baseUrl);
+        })
+        .AddHttpMessageHandler<JwtAuthorizationMessageHandler>();
 
-		services.AddValidatorsFromAssembly(typeof(Program).Assembly, includeInternalTypes: true);
+        services.AddScoped(sp =>
+            sp.GetRequiredService<IHttpClientFactory>().CreateClient("Inspekta")
+        );
+        #endregion
 
-		services.AddBlazoredToast();
-		services.AddBlazoredLocalStorage();
-		services.AddAuthorizationCore(config =>
-		{
-			foreach (var policy in AppPolicies.Policies)
-			{
-				config.AddPolicy(policy.Key, policy.Value);
-			}
-		});
+        #region Validator
+        services.AddValidatorsFromAssembly(typeof(Program).Assembly, includeInternalTypes: true);
+        #endregion
+
+        #region Toasts
+        services.AddBlazoredToast();
+        #endregion
+
+        #region Local Storage
+        services.AddBlazoredLocalStorage();
+        #endregion
+
+        #region Authorization & Authentication
+        services.AddAuthorizationCore(config =>
+        {
+            foreach (var policy in AppPolicies.Policies)
+            {
+                config.AddPolicy(policy.Key, policy.Value);
+            }
+        });
 
         services.AddScoped<JwtAuthenticationStateProvider>();
         services.AddScoped<AuthenticationStateProvider>(sp =>
             sp.GetRequiredService<JwtAuthenticationStateProvider>());
+        #endregion
+
+        #region Localization
+        services.AddLocalization(options => { options.ResourcesPath = "Resources"; });
+
+        CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pl-PL");
+        CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("pl-PL");
+        #endregion
 
         return services;
 	}
---
+++ Inspekta.Client/LocalizedComponentBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+
+namespace Inspekta.Client;
+
+public class LocalizedComponentBase : ComponentBase
+{
+    [Inject]
+    private IStringLocalizerFactory LocalizerFactory { get; set; } = default!;
+
+    private IStringLocalizer? _localizer;
+
+    protected IStringLocalizer L =>
+        _localizer ??= LocalizerFactory.Create(GetType());
+
+    protected string T(string key) => L[key];
+}
