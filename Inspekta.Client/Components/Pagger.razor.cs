5d8d54fdecff4393cdc5a298323da4c04ae32e64
Mikołaj Szczepek <m.szczepek01@gmail.com>
2026-07-09 20:18:53 +0200

    Extract footer from component to footer.razor

---
+++ Inspekta.Client/Components/Pagger.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Inspekta.Client.Components
+{
+    public partial class Pagger
+    {
+        [Parameter]
+        public int CurrentPage { get; set; }
+
+        [Parameter]
+        public int Pages { get; set; }
+
+        [Parameter]
+        public EventCallback<int> CurrentPageChanged { get; set; }
+
+        private async Task ChangePage(int page)
+        {
+            if (CurrentPageChanged.HasDelegate)
+                await CurrentPageChanged.InvokeAsync(page);
+        }
+    }
+}
