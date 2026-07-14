f1128f846b152c969db86820fb1e857b2f18f810
Mikołaj Szczepek <m.szczepek01@gmail.com>
2026-07-14 20:06:06 +0200

    Creating user mechanism
    Not done yet

--- Inspekta.Client/Components/Pages/Users/User.razor.cs
+++ Inspekta.Client/Components/Pages/Users/User.razor.cs
@@ -1,5 +1,8 @@
 ﻿using Inspekta.Shared.DTOs;
+using Inspekta.Shared.DTOs.User;
+using Inspekta.Shared.Models;
 using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
 
 namespace Inspekta.Client.Components.Pages.Users;
 
@@ -11,4 +14,35 @@ public partial class User
     public bool IsReadOnly { get; set; }
 
     private UserDto Model { get; set; } = new();
+
+    private async Task HandleValidSubmit()
+    {
+        CancellationToken ct = new();
+        HttpResponseMessage? response = await _HttpClient.PostAsJsonAsync<UserDto>("Api/Authorization/SignUp", Model, ct);
+
+        if (response is null)
+            return;
+
+        if (response.IsSuccessStatusCode)
+        {
+            UserDto? result = await response.Content.ReadFromJsonAsync<UserDto>(ct);
+            if (result is null) return;
+
+            Toast.ShowSuccess(T("user_created_successfully"));
+            Navigation.Refresh();
+        }
+        else
+        {
+            InspektaError? error = await response.Content.ReadFromJsonAsync<InspektaError>(ct);
+
+            if (error is not null)
+                Toast.ShowError(T("creating_user_failed") + T(error.Detail!));
+        }
+    }
+
+    private void HandleInvalidSubmit()
+    {
+        Console.WriteLine("Form submission failed due to validation errors.");
+        // Handle form submission failure logic here
+    }
 }
---
+++ Inspekta.Shared/DTOs/User/UserCreateDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inspekta.Shared.DTOs.User;
+
+public class UserCreateDto
+{
+    [Required(ErrorMessage = "Login nie może być pusty.")]
+    public string? Login { get; set; }
+
+    [Required(ErrorMessage = "Hasło nie może być puste.")]
+    public string? Password { get; set; }
+
+    [Required(ErrorMessage = "Hasło nie może być puste."),
+    Compare(nameof(Password), ErrorMessage = "Hasła muszą być identyczne.")]
+    public string? ConfirmPassword { get; set; }
+}
